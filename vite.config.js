19867518dd2a7ba4e24eec5f1179edcd1d93865e
Ammar-tarek <marotarek25@gmail.com>
2024-08-24 13:31:50 +0300

    up 1

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,18 @@ export default defineConfig({
       "@vime/core": "/node_modules/@vime/core",
     },
   },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Suppress warnings related to annotations that Rollup cannot interpret
+        if (
+          warning.message.includes("annotation that Rollup cannot interpret")
+        ) {
+          return;
+        }
+        // Let other warnings through
+        warn(warning);
+      },
+    },
+  },
 });
